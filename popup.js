4c0c9c4e23973af822c440744c2b58471923dd39
kessio <64524758+kessio@users.noreply.github.com>
2022-07-21 15:47:39 +0300

    Added an array of objects for portfolio

--- popup.js
+++ popup.js
@@ -1,5 +1,57 @@
-const project1Btn = document.getElementById('view-project-1');
-project1Btn.addEventListener('click', () => {
-    
-  //alert('helllo')
-})
+const portfolioDetails = [
+  {
+    id:1,
+    name: 'Tonic',
+    description: "A daily selection of privately personalized reads; no accounts or  sign-ups required",
+    moreDetails: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    img: 'images/Snapshoot%20Portfolio-desktop.png',
+    mobileImg: 'images/Snapshoot%20Portfolio-1.png',
+    technologies: ['html','css','ruby','javascript'],
+    Tags: ['Canopy','Back End Dev','2015'],
+    tagsImg: ['dot.png','dot.png'],
+    liveLink:['#'],
+    sourceLink:['#']
+  },
+  {
+    id:2,
+    name: 'Multi-Post Stories',
+    description: "Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming theirfriends.",
+    moreDetails: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    img: 'images/Snapshoot%20Portfolio-2-desktop.png',
+    mobileImg: 'images/Snapshoot%20Portfolio-2.png',
+    technologies: ['html','css','ruby','javascript'],
+    Tags: ['Facebook','Full Stack Dev','2015'],
+    tagsImg: ['dot.png','dot.png'],
+    liveLink:['#'],
+    sourceLink:['#']
+   
+  },
+  {
+    id:3,
+    name: 'Facebook 360',
+    description: " Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear   VR..",
+    moreDetails: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    img: 'images/Snapshoot%20Portfolio-3-desktop.png',
+    mobileImg: 'images/Snapshoot%20Portfolio-3.png',
+    technologies: ['html','css','ruby','javascript'],
+    Tags: ['Facebook','Full Stack Dev','2015'],
+    tagsImg: ['dot.png','dot.png'],
+    liveLink:['#'],
+    sourceLink:['#']
+   
+  },
+  {
+    id:4,
+    name: 'Uber Navigation',
+    description: "A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.",
+    moreDetails: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    img: 'images/Snapshoot%20Portfolio-4-desktop.png',
+    mobileImg: 'images/Snapshoot%20Portfolio-4.png',
+    technologies: ['html','css','ruby','javascript'],
+    Tags: ['Uber','Lead Developer','2015'],
+    tagsImg: ['images/dot.png','dot.png'],
+    liveLink:['#'],
+    sourceLink:['#']
+   
+  }
+]
