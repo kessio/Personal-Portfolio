bb59d1c2ed09ac6a43a7f9b21bed5f4a2400e253
kessio <64524758+kessio@users.noreply.github.com>
2022-07-23 12:51:37 +0300

    Set formdata

--- storage.js
+++ storage.js
@@ -2,3 +2,15 @@ const form = document.getElementById("contact-form");
 const userName = document.getElementById("name");
 const userEmail = document.getElementById("email");
 const userMessage = document.getElementById("comment");
+
+const formData = { fullName: '', email: '', userMessage: '' };
+Array.from(form.elements).forEach((input) => {
+    input.addEventListener('input', () => {
+      /* Fill our object with values   */
+      formData.fullName = userName.value;
+      formData.email = userEmail.value;
+      formData.userMessage = userMessage.value;
+      localStorage.setItem('formData', JSON.stringify(formData));
+    });
+  });
+  
