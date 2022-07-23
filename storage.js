a474f0c2beee5d5f106ec61030a46216106f5ff9
Tommymarvel <99184522+Tommymarvel@users.noreply.github.com>
2022-07-23 10:24:25 +0100

    created form variables

---
+++ storage.js
@@ -0,0 +1,4 @@
+const form = document.getElementById("contact-form");
+const userName = document.getElementById("name");
+const userEmail = document.getElementById("email");
+const userMessage = document.getElementById("comment");
