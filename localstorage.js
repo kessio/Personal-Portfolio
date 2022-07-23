682a75962619547311bd5e37d83ff4013a803aff
kessio <64524758+kessio@users.noreply.github.com>
2022-07-23 10:29:00 +0300

    Added javascript local storage

--- localstorage.js
+++ localstorage.js
@@ -1,3 +1,32 @@
+const form = document.getElementById('contact-form')
+const  userName = document.getElementById('name');
+const  userEmail = document.getElementById('email');
+const userMessage = document.getElementById('comment');
+
+ const formData = {fullName: '' , email: '', userMessage: ''}
+
+
+Array.from(form.elements).forEach(input => {
+  input.addEventListener('input', () => {
+    /* Fill our object with values   */
+    formData.fullName = userName.value;
+    formData.email = userEmail.value;
+    formData.userMessage = userMessage.value;
+    /* set form data */
+    localStorage.setItem('formData', JSON.stringify(formData));
+   
+   
+  })
+
+})
+/* get form data and set to our input fiedl value */
+let getFormData = localStorage.getItem('formData');
+if (getFormData) {
+  getFormData = JSON.parse(getFormData);
+  userName.value = getFormData.fullName;
+  userEmail.value = getFormData.email;
+  userMessage.value = getFormData.userMessage;
+}
 
 
 
